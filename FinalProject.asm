bb3161a0fa2111656bc930c6c93397629a11b166
Mrinank <mrinank.sivakumar@ontariotechu.net>
2020-07-04 00:25:08 -0400

    Update 0.0.1
    - Added exit macro
    - Added print macros
    - Added input macros

--- FinalProject.asm
+++ FinalProject.asm
@@ -5,7 +5,57 @@
 .text
 	main:
 		# Macros
-		
+		.macro exit # Exits the program
+		li $v0, 10
+		syscall
+		.end_macro
+		.macro print_int (%int) # Prints an integer
+		li $v0, 1
+		add $a0, $zero, %int
+		syscall
+		.end_macro
+		.macro print_float (%flt) # Prints a float
+		li $v0, 2
+		add $f12, $zero, %flt
+		syscall
+		.end_macro
+		.macro print_double (%dbl) # Prints a double
+		li $v0, 3
+		add $f12, $zero, %dbl
+		syscall
+		.end_macro
+		.macro print_str (%str) # Prints a string
+		li $v0, 4
+		add $a0, $zero, %str
+		syscall
+		.end_macro
+		.macro print_char (%char) # Prints a character
+		li $v0, 11
+		add $a0, $zero, %char
+		syscall
+		.end_macro
+		.macro input_int # Gets integer input ; stored in $v0
+		li $v0, 5
+		syscall
+		.end_macro
+		.macro input_float # Gets float input ; stored in $f0
+		li $v0, 6
+		syscall
+		.end_macro
+		.macro input_double # Gets double input ; stored in $f0
+		li $v0, 7
+		syscall
+		.end_macro
+		.macro input_str (%var, %x) # Gets atring input    ############################################
+		li $v0, 8					    #
+		la $a0, %var					    # Not sure about this one
+		li $a1, %x					    # Will need to be tested
+		syscall						    #
+		.end_macro					    ############################################
+		.macro input_char # Gets character input ; stored in $v0
+		li $v0, 12
+		syscall
+		.end_macro
 		
 		loop:
 			li $v0, 4
@@ -45,5 +95,4 @@
 			else:
 				j loop
 		end:
-	li $v0, 10
-	syscall
+	exit
